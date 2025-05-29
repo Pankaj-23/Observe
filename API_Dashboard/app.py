bce996d993c68f616983b899490b6a85faeac596
Pankaj <64484582+Pankaj-23@users.noreply.github.com>
2025-05-30 00:19:53 +0530

    Update app.py

--- API_Dashboard/app.py
+++ API_Dashboard/app.py
@@ -4,8 +4,8 @@ import requests
 app= Flask(__name__)
 @app.route("/")
 def github_user():
-  username="Octocat"
-  url=f"https://api.github.com/Pankaj-23/Observe/edit/master/API_Dashboard/"
+  username="Pankaj-23"
+  url=f"https://api.github.com/users/Pankaj-23"
   response = requests.get(url)
   
   if response.status_code == 200:
