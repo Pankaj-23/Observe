c976f050542df58337b3fce0547dcb9a70d3363c
Pankaj <64484582+Pankaj-23@users.noreply.github.com>
2025-05-29 23:53:35 +0530

    Update app.py

--- API_Dashboard/app.py
+++ API_Dashboard/app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template
+import requests
+
+app= Flask(__name__)
+@app.route("/")
+def github_user():
+  username="Octocat"
+  url=f"https://api.github.com/Pankaj-23/Observe/edit/master/API_Dashboard/"
+  response = requests.get(url)
+  
+  if response.status_code == 200:
+    data = response.json()
+    return render_template(dashboard.html, user=data)
+  else:
+    return f"Error: {reponse.status_code}"
+
+if __name__ == "__main__"
+app.run(host="0.0.0.0", port=5000)
 
