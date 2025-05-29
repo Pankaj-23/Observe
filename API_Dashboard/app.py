d3599679c9876132a9b08ebda28b5b1d0c71e6c6
Pankaj <64484582+Pankaj-23@users.noreply.github.com>
2025-05-30 01:18:50 +0530

    Update app.py

--- API_Dashboard/app.py
+++ API_Dashboard/app.py
@@ -15,5 +15,5 @@ def github_user():
     return f"Error: {reponse.status_code}"
 
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000, debug=True)
+  app.run(host="0.0.0.0", port=5000, debug=True)
 
